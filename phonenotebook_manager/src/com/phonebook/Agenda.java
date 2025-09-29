579b32325dbe7cc762e998e3061243e59f1a4392
Jennifer Martínez <jennifertavera05@gmail.com>
2025-09-29 12:37:44 -0600

    feat: add method AñadirContacto to Agenda class

--- phonenotebook_manager/src/com/phonebook/Agenda.java
+++ phonenotebook_manager/src/com/phonebook/Agenda.java
@@ -1,5 +1,41 @@
 package com.phonebook;
 
-public class Agenda {
+import java.util.HashMap;
+import java.util.Map;
 
+public class Agenda {
+	private Map<String, Contacto> contactos;
+	private int limiteContactos;
+	
+	public Agenda() {
+        this(10);
+    }
+
+    public Agenda(int limiteContactos) {
+        this.limiteContactos = limiteContactos;
+        this.contactos = new HashMap<>();
+    }
+    
+    public void añadirContacto(Contacto c) {
+    	
+    	if(contactos.size() >= limiteContactos) {
+    		System.out.println("La agenda está llena, no puedes agregar más contactos");
+    		return;
+    	}
+    	
+    	if(contactos.containsKey(c.getNombre().trim().toLowerCase())) {
+    		System.out.println("Ya existe un contacto con ese nombre");
+    		return;
+    	}
+    	
+    	if(c.getNombre().trim().isEmpty()) {
+    		System.out.println("El nombre no puede estar vacío. Ingresa un nombre:");
+    		return;
+    	}
+    	
+    	contactos.put(c.getNombre().trim().toLowerCase(), c);
+    	System.out.println("Se agregó correctamente");
+    }
+
+	
 }
--- phonenotebook_manager/src/com/phonebook/Main.java
+++ phonenotebook_manager/src/com/phonebook/Main.java
@@ -3,7 +3,10 @@ package com.phonebook;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Contacto contacto = new Contacto("", "123456878");
+		Agenda agenda = new Agenda();
+		
+		agenda.añadirContacto(contacto);
 
 	}
 
