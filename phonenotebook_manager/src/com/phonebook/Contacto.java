bf1ccca144fc453c91abc6e0556fb9141806d2bd
Jennifer Martínez <jennifertavera05@gmail.com>
2025-09-29 11:37:41 -0600

    feat: add attributes for class Contact

--- phonenotebook_manager/src/com/phonebook/Contacto.java
+++ phonenotebook_manager/src/com/phonebook/Contacto.java
@@ -1,5 +1,43 @@
 package com.phonebook;
 
 public class Contacto {
+	private String nombre;
+	private String telefono;
+	
+	public Contacto() {
+		
+	}
+	
+	public Contacto(String nombre, String telefono) {
+		this.nombre = nombre;
+		this.telefono = telefono;
+	}
 
+	/**
+	 * @return the nombre
+	 */
+	public String getNombre() {
+		return nombre;
+	}
+
+	/**
+	 * @param nombre the nombre to set
+	 */
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	/**
+	 * @return the telefono
+	 */
+	public String getTelefono() {
+		return telefono;
+	}
+
+	/**
+	 * @param telefono the telefono to set
+	 */
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
 }
