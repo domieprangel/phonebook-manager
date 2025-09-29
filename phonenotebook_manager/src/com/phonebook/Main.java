d75d5f82cafe7ec7455abae5e8faee86d2f1d94b
domieprangel <dominiquerangelperedo@gmail.com>
2025-09-29 16:37:19 -0600

    feat: add menu programming to main class

--- phonenotebook_manager/src/com/phonebook/Main.java
+++ phonenotebook_manager/src/com/phonebook/Main.java
@@ -1,26 +1,52 @@
 package com.phonebook;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		Contacto contacto = new Contacto("Dom Ran", "123456878");
-		Contacto contacto2 = new Contacto("Jaime", "123456878");
-		Contacto contacto3 = new Contacto("Jen", "123456878");
-		Agenda agenda = new Agenda();
+		Scanner sc = new Scanner(System.in);
+		Agenda contactos = new Agenda();
+		int numMenu = 0;
+		String nombre;
+		String apellido;
+		String telefono;
+		do {
+			System.out.println("MENÚ DE AGENDA TELEFÓNICA");
+			System.out.println("1. Agregar contacto nuevo");
+			System.out.println("2. Buscar contacto");
+			System.out.println("3. Mostrar contactos guardados");
+			System.out.println("4. Modificar teléfono de contacto");
+			System.out.println("5. Eliminar contacto");
+			System.out.println("6. Salir del menú");
+			System.out.print("Escribe el número de la acción que deseas realizar: ");
+			numMenu = sc.nextInt();	
+			sc.nextLine();
+			switch (numMenu) {
+			case 1:
+				System.out.println("Nombre: ");
+				nombre = sc.nextLine();
+				System.out.println("Apellido: ");
+				apellido = sc.nextLine();
+				System.out.println("Teléfono: ");
+				telefono = sc.nextLine();
+				Contacto contactoNuevo = new Contacto(nombre, apellido, telefono);
+				contactos.añadirContacto(contactoNuevo);
+				break;
+			case 2:
+				System.out.println("Nombre: ");
+				nombre = sc.nextLine();
+				System.out.println("Apellido: ");
+				apellido = sc.nextLine();
+				contactos.buscaContacto(nombre,apellido);
+				break;
+			}
+			
+			System.out.print("Escribe el número de la acción que deseas realizar: ");
+			numMenu = sc.nextInt();	
+		}while(numMenu != 6);
+		System.out.println("Saliste de tu Agenda Telefónica");
 		
-		agenda.añadirContacto(contacto);
-		agenda.añadirContacto(contacto2);
-		agenda.añadirContacto(contacto3);
-		
-		
-		
-		
-		agenda.listarContactos();
-		
-		agenda.buscaContacto("JaaaimE");
-		agenda.modificarTelefono("Dom", "Ran", "123");
-		agenda.espaciosLibres();
-		System.out.println(contacto);
 	
 	}
 }
